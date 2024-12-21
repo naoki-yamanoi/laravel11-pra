526fc78bfa7f13bd0edf134caae4fe60b8c983dc
naoki-yamanoi <ti44nnyy999@gmail.com>
2024-12-21 22:17:28 +0900

    first commit

---
+++ resources/views/helloWorld.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Laravel</title>
+</head>
+
+<body>
+  <p>hello world</p>
+</body>
+
+</html>
